a1bcf5eb631231a8cd773c8118990c475f4999a8
Eric St. John <ericstj@microsoft.com>
2015-08-28 07:35:43 -0700

    Fix existing sample

--- build.cmd
+++ build.cmd
@@ -1,22 +1,31 @@
 @echo off > nul
 
 REM Build ClassLibrary
-%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild SatellitePackageSample\ClassLibrary\ClassLibrary.csproj
+%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild Satellite-Packages\ClassLibrary\ClassLibrary.csproj
 
 SETLOCAL
-SET nuget=SatellitePackageSample\.nuget\NuGet.exe
+SET nugetsource=https://dist.nuget.org/win-x86-commandline/v3.1.0-beta/nuget.exe
+SET bin=%~dp0bin
+SET nuget=%bin%\NuGet.exe
+SET packages=%~dp0\packages
+if NOT EXIST %bin% md %bin%
+if NOT EXIST %packages% md %packages%
+
+if NOT EXIST %nuget% (
+  powershell -Command "(New-Object System.Net.WebClient).DownloadFile(\"%nugetsource%\", \"%nuget%\")"
+)
 
 REM Build the NuGet packages from ClassLibrary
-%nuget% pack SatellitePackageSample\ClassLibrary\ClassLibrary.nuspec
-%nuget% pack SatellitePackageSample\ClassLibrary\ClassLibrary.ja-jp.nuspec
-%nuget% pack SatellitePackageSample\ClassLibrary\ClassLibrary.ru-ru.nuspec
-%nuget% pack SatellitePackageSample\ClassLibrary\ClassLibrary.cs.nuspec
-%nuget% pack SatellitePackageSample\ClassLibrary\ClassLibrary.Localization.nuspec
+%nuget% pack Satellite-Packages\ClassLibrary\ClassLibrary.nuspec -OutputDirectory %packages%
+%nuget% pack Satellite-Packages\ClassLibrary\ClassLibrary.ja-jp.nuspec -OutputDirectory %packages%
+%nuget% pack Satellite-Packages\ClassLibrary\ClassLibrary.ru-ru.nuspec -OutputDirectory %packages%
+%nuget% pack Satellite-Packages\ClassLibrary\ClassLibrary.cs.nuspec -OutputDirectory %packages%
+%nuget% pack Satellite-Packages\ClassLibrary\ClassLibrary.Localization.nuspec -OutputDirectory %packages%
 
-REM Make those packages available through a new feed source called SatellitePackageSample
-%nuget% sources add -Name SatellitePackageSample -Source "%CD%"
+REM Restore packages
+%nuget% restore Satellite-Packages\SatellitePackageSample.sln
 
 REM Build the ConsoleApp
-%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild SatellitePackageSample\ConsoleApp\ConsoleApp.csproj
+%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild Satellite-Packages\ConsoleApp\ConsoleApp.csproj
 
 ENDLOCAL
