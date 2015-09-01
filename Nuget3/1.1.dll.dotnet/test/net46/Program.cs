4322d00483681ee8aabad35ef4e397b725ae16e6
Eric St. John <ericstj@microsoft.com>
2015-08-31 17:11:32 -0700

    Refactor samples build and add a first sample for nuget3 walkthrough

---
+++ Nuget3/1.1.dll.dotnet/test/net46/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(System.Banana.FruitTree.GetBananas());
+        }
+    }
+}
--- build.cmd
+++ build.cmd
@@ -1,31 +1,30 @@
 @echo off > nul
 
-REM Build ClassLibrary
-%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild Satellite-Packages\ClassLibrary\ClassLibrary.csproj
-
 SETLOCAL
-SET nugetsource=https://dist.nuget.org/win-x86-commandline/v3.1.0-beta/nuget.exe
+SET MSBuild=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
 SET bin=%~dp0bin
-SET nuget=%bin%\NuGet.exe
-SET packages=%~dp0\packages
-if NOT EXIST %bin% md %bin%
-if NOT EXIST %packages% md %packages%
+SET NuGet=%bin%\NuGet.exe
+SET PackageOutputDir=%bin%\packages
+SET NuGetPackages=%~dp0\packages
+SET NuGetSource=https://dist.nuget.org/win-x86-commandline/v3.1.0-beta/nuget.exe
 
-if NOT EXIST %nuget% (
-  powershell -Command "(New-Object System.Net.WebClient).DownloadFile(\"%nugetsource%\", \"%nuget%\")"
+if NOT EXIST %bin% md %bin%
+if NOT EXIST %NuGetPackages% md %NuGetPackages%
+
+if NOT EXIST "%MSBuild%" (
+  echo MSBuild 14 is required, please ensure you've installed Visual Studio 2015.
+  exit /b 1
 )
 
-REM Build the NuGet packages from ClassLibrary
-%nuget% pack Satellite-Packages\ClassLibrary\ClassLibrary.nuspec -OutputDirectory %packages%
-%nuget% pack Satellite-Packages\ClassLibrary\ClassLibrary.ja-jp.nuspec -OutputDirectory %packages%
-%nuget% pack Satellite-Packages\ClassLibrary\ClassLibrary.ru-ru.nuspec -OutputDirectory %packages%
-%nuget% pack Satellite-Packages\ClassLibrary\ClassLibrary.cs.nuspec -OutputDirectory %packages%
-%nuget% pack Satellite-Packages\ClassLibrary\ClassLibrary.Localization.nuspec -OutputDirectory %packages%
+if NOT EXIST "%NuGet%" (
+  powershell -Command "(New-Object System.Net.WebClient).DownloadFile(\"%nugetsource%\", \"%NuGet%\")"
+)
 
-REM Restore packages
-%nuget% restore Satellite-Packages\SatellitePackageSample.sln
+"%MSBuild%" restore-src.proj
+"%MSBuild%" build-src.proj
+"%MSBuild%" build-packages.proj
+"%MSBuild%" restore-test.proj
+"%MSBuild%" build-test.proj
 
-REM Build the ConsoleApp
-%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild Satellite-Packages\ConsoleApp\ConsoleApp.csproj
 
 ENDLOCAL
